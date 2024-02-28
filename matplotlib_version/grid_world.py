82f48ffbe04e56365dcbb9a42e1658b7fa479a53
Patrick Singer <60189276+PatrickSinger99@users.noreply.github.com>
2024-02-28 14:55:50 +0100

    ray update

--- matplotlib_version/grid_world.py
+++ matplotlib_version/grid_world.py
@@ -1,9 +1,23 @@
 import math
 import matplotlib.pyplot as plt
 from matplotlib import patches
+from typing import Union
+
+
+class Ray:
+    def __init__(self, start_x: int, start_y: int, angle_degrees: Union[int, float],
+                 length: Union[int, float, None] = None):
+        self.start_x = start_x
+        self.start_y = start_y
+        self.angle = angle_degrees
+        self.length = length
 
 
 class GridWorld:
+
+    cell_borders_color = "grey"
+    cell_value_colors = {1: "black"}
+
     def __init__(self, cell_amount_x: int, cell_amount_y: int, cell_size: int = 1000):
         self.cell_amount_x = cell_amount_x
         self.cell_amount_y = cell_amount_y
@@ -11,6 +25,7 @@ class GridWorld:
 
         # Initialize grid
         self.grid = [[0 for _ in range(self.cell_amount_x)] for _ in range(self.cell_amount_y)]
+        self.rays = []
 
     def __str__(self):
         return_str = ""
@@ -43,32 +58,41 @@ class GridWorld:
         for i in range(length_y):
             self.set_value_line(start_x, start_y + i, length_x, "h", value)
 
-    def plot(self, size: int = 5, show_cell_borders: bool = True, show_cell_states: bool = True):
+    def add_ray(self, start_x, start_y, angle_degrees, length=None):
+        new_ray = Ray(start_x, start_y, angle_degrees, length)
+        self.rays.append(new_ray)
 
-        plt.figure(figsize=(int(size * (self.cell_amount_x / self.cell_amount_y)), size))
-        fig, ax = plt.subplots()
+    def plot(self, size: int = 5, show_cell_borders: bool = True, show_cell_states: bool = True, show_rays: bool = True):
+
+        fig, ax = plt.subplots(figsize=(int(size * (self.cell_amount_x / self.cell_amount_y)), size))
 
         if show_cell_states:
             for y_index, x_row in enumerate(self.grid):
                 for x_index, cell_value in enumerate(x_row):
                     if cell_value != 0:
                         ax.add_patch(patches.Rectangle((x_index * self.cell_size - 1, y_index * self.cell_size - 1),
-                                                       self.cell_size, self.cell_size, color="black"))
+                                                       self.cell_size, self.cell_size,
+                                                       color=GridWorld.cell_value_colors[cell_value]))
 
         if show_cell_borders:
             for i in range(self.cell_amount_y + 1):
-                plt.axhline(i * self.cell_size, color="grey", linestyle="dashed", linewidth="1")
+                plt.axhline(i * self.cell_size, color=GridWorld.cell_borders_color, linestyle="dashed", linewidth="1")
 
             for i in range(self.cell_amount_x + 1):
-                plt.axvline(i * self.cell_size, color="grey", linestyle="dashed", linewidth="1")
+                plt.axvline(i * self.cell_size, color=GridWorld.cell_borders_color, linestyle="dashed", linewidth="1")
 
+        if show_rays:
+            for ray in self.rays:
+                pass  # TODO
+        
         plt.xlim(0, self.cell_amount_x * self.cell_size)
         plt.ylim(0, self.cell_amount_y * self.cell_size)
 
         ax.xaxis.tick_top()
         plt.gca().invert_yaxis()
         plt.gca().set_aspect('equal')
-        plt.show()
+
+        return fig
 
 
 if __name__ == '__main__':
@@ -80,5 +104,8 @@ if __name__ == '__main__':
     gw.set_value_block(4, 7, 4, 2, 1)
     gw.set_value(7, 6, 1)
 
+    gw.add_ray(1800, 3200, 45)
+
     print(gw)
     gw.plot()
+    plt.show()
