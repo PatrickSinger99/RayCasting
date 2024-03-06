4ecc6ee2f889f0196b53b56d6d8a97a16aa4b49a
Patrick Singer <60189276+PatrickSinger99@users.noreply.github.com>
2024-03-06 16:25:57 +0100

    fix horizontal col calc

--- matplotlib_version/grid_world.py
+++ matplotlib_version/grid_world.py
@@ -5,24 +5,40 @@ from typing import Union, Type
 
 
 class Ray:
-    def __init__(self, start_x: int, start_y: int, angle_degrees: Union[int, float],
-                 length: Union[int, float, None] = None):
+    def __init__(self, start_x: int, start_y: int, angle_degrees: Union[int, float]):
+        # Start Parameters
         self.start_x = start_x
         self.start_y = start_y
         self.angle = angle_degrees
-        self.length = length
 
+        # Calculated Parameters
+        self.horizontal_grid_collisions = []
+        self.vertical_grid_collisions = []
+        self.collided_cell_x = None  # Collision coordiante
+        self.collided_cell_y = None  # Collision coordiante
+        self.collided_cell_x_pos = None  # Cell position in grid
+        self.collided_cell_y_pos = None  # Cell position in grid
+
+    # TODO TEMP
     def calculate_direction(self, distance=1):
         angle_radians = math.radians(self.angle)
         new_x = self.start_x + distance * math.cos(angle_radians)
         new_y = self.start_y + distance * math.sin(angle_radians)
         return new_x, new_y
 
-    def calculate_grid_collision(self, grid_class_object):  # TODO: Why is "grid: Type[GridWorld]" not working ??
+    def calculate_grid_collision(self, grid_class_object):
+
+        # Reset calculated parameters
+        self.horizontal_grid_collisions = []
+        self.vertical_grid_collisions = []
+        self.collided_cell_x = None  # Collision coordiante
+        self.collided_cell_y = None  # Collision coordiante
+        self.collided_cell_x_pos = None  # Cell position in grid
+        self.collided_cell_y_pos = None  # Cell position in grid
+
+        # Precalculate sine and cosine
         cosine_angle = math.cos(math.radians(self.angle))
-        print(cosine_angle)
-        horizontal_grid_collisions = []
-        vertical_grid_collisions = []
+        sine_angle = math.sin(math.radians(self.angle))
 
         """Get coordinates of first collision with horizontal grid border"""
 
@@ -32,23 +48,44 @@ class Ray:
 
         # Use next horizontal border to calculate distance to it (opposite) and derive adjacent after
         opposite = next_horizontal_border - self.start_y  # Gegenkathete
-        adjacent = opposite / cosine_angle  # Ankathete
+        adjacent = opposite / sine_angle  # Ankathete
 
         # With adjacent, calculate the x coordinate of the grid collision (y coordinate is simply the horizontal border)
-        first_horizontal_grid_collision = (self.start_x + adjacent, next_horizontal_border)
-        horizontal_grid_collisions.append(first_horizontal_grid_collision)
+        first_horizontal_grid_collision = (self.start_x + adjacent * cosine_angle, next_horizontal_border)
+        self.horizontal_grid_collisions.append(first_horizontal_grid_collision)
 
         """Get subsequent horizontal grid collisions (From now on always full cell steps)"""
+
         current_x, current_y = first_horizontal_grid_collision
-        full_cell_horizontal_collsion_x_length = cell_size / cosine_angle  # opposite calculation
+        full_cell_horizontal_collision_x_length = cell_size / sine_angle  # opposite calculation
+
+        # Colcualte world boundaries
+        y_boundary = grid_class_object.cell_amount_y * cell_size
+        x_boundary = grid_class_object.cell_amount_x * cell_size
 
         # Step through horizontal grid lines until a collision with a cell or the end of the world
-        last_y_row = grid_class_object.cell_amount_y * cell_size
-        while current_y < last_y_row:
-            current_x, current_y = current_x + full_cell_horizontal_collsion_x_length, current_y + cell_size
-            horizontal_grid_collisions.append((current_x, current_y))
+        while 0 < current_y < y_boundary and 0 < current_x < x_boundary:
+            current_x += full_cell_horizontal_collision_x_length * cosine_angle
+            current_y += cell_size
 
-        return horizontal_grid_collisions
+            # Check collision with cell
+            cell_y_coord = int(current_y / cell_size)
+            cell_x_coord = math.floor(current_x / cell_size)
+            try:
+                if grid_class_object.get_value(cell_x_coord, cell_y_coord) != 0:
+
+                    # Save values for collision with cell
+                    self.collided_cell_x = current_x
+                    self.collided_cell_y = current_y
+                    self.collided_cell_x_pos = cell_x_coord
+                    self.collided_cell_y_pos = cell_y_coord
+                    break
+
+                else:
+                    self.horizontal_grid_collisions.append((current_x, current_y))
+
+            except IndexError:
+                break
 
 
 class GridWorld:
@@ -96,8 +133,8 @@ class GridWorld:
         for i in range(length_y):
             self.set_value_line(start_x, start_y + i, length_x, "h", value)
 
-    def add_ray(self, start_x, start_y, angle_degrees, length=None):
-        new_ray = Ray(start_x, start_y, angle_degrees, length)
+    def add_ray(self, start_x, start_y, angle_degrees):
+        new_ray = Ray(start_x, start_y, angle_degrees)
         self.rays.append(new_ray)
 
     def plot(self, size: int = 5, show_cell_borders: bool = True, show_cell_states: bool = True, show_rays: bool = True):
@@ -122,13 +159,18 @@ class GridWorld:
         if show_rays:
             for ray in self.rays:
                 # TEST
-                end_x, end_y = ray.calculate_direction(distance=100000)
-                ax.add_line(lines.Line2D([ray.start_x, end_x], [ray.start_y, end_y]))
+                ray.calculate_grid_collision(grid_class_object=self)
 
-                coll_coord_list = ray.calculate_grid_collision(grid_class_object=self)
-                print(coll_coord_list)
-                for x, y in coll_coord_list:
-                    ax.plot(x, y, marker='x', color="red")
+                for x, y in ray.horizontal_grid_collisions:
+                    ax.plot(x, y, marker='.', color="red")
+
+                if ray.collided_cell_x is not None:
+                    ax.plot(ray.collided_cell_x, ray.collided_cell_y, marker='x', color="lime")
+                    end_x, end_y = ray.collided_cell_x, ray.collided_cell_y
+                else:
+                    end_x, end_y = ray.calculate_direction(distance=100000)
+
+                ax.add_line(lines.Line2D([ray.start_x, end_x], [ray.start_y, end_y]))
 
         plt.xlim(0, self.cell_amount_x * self.cell_size)
         plt.ylim(0, self.cell_amount_y * self.cell_size)
@@ -149,7 +191,18 @@ if __name__ == '__main__':
     gw.set_value_block(4, 7, 4, 2, 1)
     gw.set_value(7, 6, 1)
 
-    gw.add_ray(2800, 4200, 45)
+
+    for i in range(10):
+        gw.add_ray(2800, 4200, 15 + 4 * i)
+
+    gw.add_ray(2800, 4200, 125)
+
+    gw.add_ray(2800, 4200, 115)
+
+    gw.add_ray(2800, 4200, 180)
+    gw.add_ray(2800, 4200, 190)
+
+
 
     print(gw)
     gw.plot()
