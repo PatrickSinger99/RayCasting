7e8176e57b6da91343161467e5d2b3f8b445e2d1
Patrick Singer <60189276+PatrickSinger99@users.noreply.github.com>
2024-03-12 13:35:22 +0100

    Update grid_world.py

--- matplotlib_version/grid_world.py
+++ matplotlib_version/grid_world.py
@@ -46,10 +46,10 @@ class Ray:
         else:
             upwards_direction = False
 
-        """Get coordinates of first collision with horizontal grid border"""
-
         # Calculate the next horizontal border
         cell_size = grid_class_object.cell_size
+
+        """Get coordinates of first collision with horizontal grid border"""
 
         if upwards_direction:
             next_horizontal_border = math.floor(self.start_y / cell_size) * cell_size
@@ -65,7 +65,6 @@ class Ray:
             first_horizontal_grid_collision = (self.start_x - adjacent * cosine_angle, next_horizontal_border)
         else:
             first_horizontal_grid_collision = (self.start_x + adjacent * cosine_angle, next_horizontal_border)
-        self.horizontal_grid_collisions.append(first_horizontal_grid_collision)
 
         """Get subsequent horizontal grid collisions (From now on always full cell steps)"""
 
@@ -101,6 +100,81 @@ class Ray:
             else:
                 current_y += cell_size
             current_x += full_cell_horizontal_collision_x_length * cosine_angle
+
+    def BROKEN_calculate_grid_collision(self, grid_class_object):
+
+        # Reset calculated parameters
+        self.vertical_grid_collisions = []
+        self.collision_coord_x = None  # Collision coordiante
+        self.collision_coord_y = None  # Collision coordiante
+        self.collided_cell_x = None  # Cell position in grid
+        self.collided_cell_y = None  # Cell position in grid
+
+        # Precalculate sine and cosine
+        cosine_angle = math.cos(math.radians(self.angle))
+        sine_angle = math.sin(math.radians(self.angle))
+
+        # Determine line type (pointing upwards or downwards)
+        if 90 < self.angle < 270:
+            right_direction = True
+        else:
+            right_direction = False
+
+        # Calculate the next horizontal border
+        cell_size = grid_class_object.cell_size
+
+        """Get coordinates of first collision with horizontal grid border"""
+
+        if right_direction:
+            next_vertical_border = math.floor(self.start_x / cell_size) * cell_size
+        else:
+            next_vertical_border = math.ceil(self.start_x / cell_size) * cell_size
+
+        # Use next horizontal border to calculate distance to it (opposite) and derive adjacent after
+        opposite = next_vertical_border - self.start_x  # Gegenkathete
+        adjacent = opposite / sine_angle  # Ankathete
+
+        # With adjacent, calculate the x coordinate of the grid collision (y coordinate is simply the horizontal border)
+        if right_direction:
+            first_vertical_grid_collision = (self.start_y - adjacent * cosine_angle, next_vertical_border)
+        else:
+            first_vertical_grid_collision = (self.start_y + adjacent * cosine_angle, next_vertical_border)
+
+        """Get subsequent horizontal grid collisions (From now on always full cell steps)"""
+
+        current_x, current_y = first_vertical_grid_collision
+        full_cell_vertical_collision_x_length = cell_size / sine_angle  # opposite calculation
+
+        # Calculate world boundaries
+        y_boundary = grid_class_object.cell_amount_y * cell_size
+        x_boundary = grid_class_object.cell_amount_x * cell_size
+
+        # Step through horizontal grid lines until a collision with a cell or the end of the world
+        while 0 < current_y < y_boundary and 0 < current_x < x_boundary:
+
+            # Check collision with cell
+            cell_y_coord = math.floor(current_y / cell_size)
+            cell_x_coord = int(current_x / cell_size) - (1 if right_direction else 0)
+
+            if grid_class_object.get_value(cell_x_coord, cell_y_coord) != 0:
+
+                # Save values for collision with cell
+                self.collision_coord_x = current_x
+                self.collision_coord_y = current_y
+                self.collided_cell_x = cell_x_coord
+                self.collided_cell_y = cell_y_coord
+                break
+
+            else:
+                self.vertical_grid_collisions.append((current_x, current_y))
+
+            # Increment coordinates for collision check
+            if right_direction:
+                current_x -= cell_size
+            else:
+                current_x += cell_size
+            current_y += full_cell_vertical_collision_x_length * cosine_angle
+
 
 class GridWorld:
 
@@ -178,13 +252,14 @@ class GridWorld:
                 for x, y in ray.horizontal_grid_collisions:
                     ax.plot(x, y, marker='.', color="red")
 
+                for x, y in ray.vertical_grid_collisions:
+                    ax.plot(x, y, marker='.', color="red")
+
                 if ray.collision_coord_x is not None:
                     ax.plot(ray.collision_coord_x, ray.collision_coord_y, marker='x', color="lime")
                     end_x, end_y = ray.collision_coord_x, ray.collision_coord_y
-                else:
-                    end_x, end_y = ray.calculate_direction(distance=100000)
+                    ax.add_line(lines.Line2D([ray.start_x, end_x], [ray.start_y, end_y]))
 
-                ax.add_line(lines.Line2D([ray.start_x, end_x], [ray.start_y, end_y]))
 
         plt.xlim(0, self.cell_amount_x * self.cell_size)
         plt.ylim(0, self.cell_amount_y * self.cell_size)
